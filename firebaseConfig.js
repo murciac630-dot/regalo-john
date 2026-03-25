0f35206acab7490d9b103cf2fe46cd93b4051c23
Cristian Murcia <murciac630@gmail.com>
2026-03-24 21:32:57 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,29 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCh0ArGzLJhCprULRVX2DxJB9jZrvKN9Ao",
-  authDomain: "regalo-john.firebaseapp.com",
-  projectId: "regalo-john",
-  storageBucket: "regalo-john.firebasestorage.app",
-  messagingSenderId: "693989774085",
-  appId: "1:693989774085:web:ecce861d8c44ffecb81d6c",
-  measurementId: "G-BQMBS4ZLW7"
-};
-
-// Initialize Firebase
-// Importamos las funciones adicionales que necesitamos
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "tu-id",
+  appId: "tu-app-id"
+};
 
-// Exportamos las herramientas para usarlas en otros componentes
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
